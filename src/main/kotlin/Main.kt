a77532f7055f8eb898ffc6be9ec349084151c721
vamboo26 <vamboo26@gmail.com>
2025-04-24 22:04:30 +0900

    TaxBurdenType enum 정의, 케이스에 따른 함수 분리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -53,98 +53,114 @@ interface Calculator {
     fun calculate(summary: Input): Output
 }
 
+enum class WithholdingTaxBurdenType {
+    PAYEE,
+    PAYER,
+    ;
+
+    companion object {
+        fun from(value: String): WithholdingTaxBurdenType {
+            return entries.firstOrNull { it.name.equals(value, ignoreCase = true) }
+                ?: throw IllegalArgumentException("Invalid withholding tax burden type: $value")
+        }
+    }
+}
+
 object MyCalculator : Calculator {
 
     override fun calculate(summary: Input): Output {
 
-        when (summary.withholdingTaxBurdenType) {
-            "PAYEE" -> {
-                val foreignAmountScaleByCurrency = if (summary.currency == "JPY") 0 else 2
-                val krwRoundedNrFee = summary.neighboringCopyrightFee.setScale(0, RoundingMode.HALF_UP)
-                val foreignTotalPayment =
-                    krwRoundedNrFee.divide(summary.exchangeRate, foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
-                val foreignIncomeTax = foreignTotalPayment.multiply(summary.withholdingTaxRate)
-                    .divide(BigDecimal(1.1), foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
-                val foreignResidentTax = foreignIncomeTax.multiply(BigDecimal(0.1))
-                    .setScale(foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
-                val foreignNetPayment = foreignTotalPayment.subtract(foreignIncomeTax).subtract(foreignResidentTax)
-                val krwTotalPayment =
-                    foreignTotalPayment.multiply(summary.exchangeRate).setScale(0, RoundingMode.HALF_UP)
-                val krwIncomeTax = foreignIncomeTax.multiply(summary.exchangeRate).setScale(0, RoundingMode.HALF_UP)
-                val krwResidentTax = foreignResidentTax.multiply(summary.exchangeRate).setScale(0, RoundingMode.HALF_UP)
-                val krwNetPayment = krwTotalPayment.subtract(krwIncomeTax).subtract(krwResidentTax)
-                val currencyTranslationProfit = krwTotalPayment.subtract(krwRoundedNrFee).toInt()
+        val taxBurdenType = WithholdingTaxBurdenType.from(summary.withholdingTaxBurdenType)
 
-                return Output(
-                    summary.settlementMonth,
-                    summary.rightHolderName,
-                    summary.neighboringCopyrightFee,
-                    summary.currency,
-                    summary.exchangeRate,
-                    summary.nation,
-                    summary.withholdingTaxBurdenType,
-                    summary.withholdingTaxRate,
-                    krwRoundedNrFee.divide(summary.exchangeRate, foreignAmountScaleByCurrency, RoundingMode.HALF_UP),
-                    foreignIncomeTax,
-                    foreignResidentTax,
-                    foreignNetPayment,
-                    currencyTranslationProfit,
-                    krwRoundedNrFee,
-                    krwTotalPayment,
-                    krwIncomeTax,
-                    krwResidentTax,
-                    krwNetPayment,
-                )
-            }
-
-            "PAYER" -> {
-                val foreignAmountScaleByCurrency = if (summary.currency == "JPY") 0 else 2
-                val krwRoundedNrFee = summary.neighboringCopyrightFee.setScale(0, RoundingMode.HALF_UP)
-                val foreignNetPayment =
-                    krwRoundedNrFee.divide(summary.exchangeRate, foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
-                val foreignWithHoldingTax: BigDecimal =
-                    foreignNetPayment.divide(BigDecimal(0.85), foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
-                        .subtract(foreignNetPayment)
-                val foreignIncomeTax = foreignWithHoldingTax.divide(
-                    BigDecimal(1.1),
-                    foreignAmountScaleByCurrency,
-                    RoundingMode.HALF_UP
-                )
-                val foreignResidentTax = foreignIncomeTax.multiply(BigDecimal(0.1))
-                    .setScale(foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
-                val foreignTotalPayment = foreignNetPayment.add(foreignIncomeTax).add(foreignResidentTax)
-                val krwNetPayment = foreignNetPayment.multiply(summary.exchangeRate)
-                    .setScale(0, RoundingMode.HALF_UP)
-                val krwIncomeTax = foreignIncomeTax.multiply(summary.exchangeRate)
-                    .setScale(0, RoundingMode.HALF_UP)
-                val krwResidentTax = foreignResidentTax.multiply(summary.exchangeRate)
-                    .setScale(0, RoundingMode.HALF_UP)
-                val krwTotalPayment = krwNetPayment.add(krwIncomeTax).add(krwResidentTax)
-                val currencyTranslationProfit = krwNetPayment.subtract(krwRoundedNrFee).toInt()
-
-                return Output(
-                    summary.settlementMonth,
-                    summary.rightHolderName,
-                    summary.neighboringCopyrightFee,
-                    summary.currency,
-                    summary.exchangeRate,
-                    summary.nation,
-                    summary.withholdingTaxBurdenType,
-                    summary.withholdingTaxRate,
-                    foreignTotalPayment,
-                    foreignIncomeTax,
-                    foreignResidentTax,
-                    foreignNetPayment,
-                    currencyTranslationProfit,
-                    krwRoundedNrFee,
-                    krwTotalPayment,
-                    krwIncomeTax,
-                    krwResidentTax,
-                    krwNetPayment,
-                )
-            }
-
-            else -> throw IllegalArgumentException("Invalid input")
+        return when (taxBurdenType) {
+            WithholdingTaxBurdenType.PAYEE -> calculatePayee(summary)
+            WithholdingTaxBurdenType.PAYER -> calculatePayer(summary)
         }
+    }
+
+    private fun calculatePayee(summary: Input): Output {
+        val foreignAmountScaleByCurrency = if (summary.currency == "JPY") 0 else 2
+        val krwRoundedNrFee = summary.neighboringCopyrightFee.setScale(0, RoundingMode.HALF_UP)
+        val foreignTotalPayment =
+            krwRoundedNrFee.divide(summary.exchangeRate, foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
+        val foreignIncomeTax = foreignTotalPayment.multiply(summary.withholdingTaxRate)
+            .divide(BigDecimal(1.1), foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
+        val foreignResidentTax = foreignIncomeTax.multiply(BigDecimal(0.1))
+            .setScale(foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
+        val foreignNetPayment = foreignTotalPayment.subtract(foreignIncomeTax).subtract(foreignResidentTax)
+        val krwTotalPayment =
+            foreignTotalPayment.multiply(summary.exchangeRate).setScale(0, RoundingMode.HALF_UP)
+        val krwIncomeTax = foreignIncomeTax.multiply(summary.exchangeRate).setScale(0, RoundingMode.HALF_UP)
+        val krwResidentTax = foreignResidentTax.multiply(summary.exchangeRate).setScale(0, RoundingMode.HALF_UP)
+        val krwNetPayment = krwTotalPayment.subtract(krwIncomeTax).subtract(krwResidentTax)
+        val currencyTranslationProfit = krwTotalPayment.subtract(krwRoundedNrFee).toInt()
+
+        return Output(
+            summary.settlementMonth,
+            summary.rightHolderName,
+            summary.neighboringCopyrightFee,
+            summary.currency,
+            summary.exchangeRate,
+            summary.nation,
+            summary.withholdingTaxBurdenType,
+            summary.withholdingTaxRate,
+            krwRoundedNrFee.divide(summary.exchangeRate, foreignAmountScaleByCurrency, RoundingMode.HALF_UP),
+            foreignIncomeTax,
+            foreignResidentTax,
+            foreignNetPayment,
+            currencyTranslationProfit,
+            krwRoundedNrFee,
+            krwTotalPayment,
+            krwIncomeTax,
+            krwResidentTax,
+            krwNetPayment,
+        )
+    }
+
+    private fun calculatePayer(summary: Input): Output {
+        val foreignAmountScaleByCurrency = if (summary.currency == "JPY") 0 else 2
+        val krwRoundedNrFee = summary.neighboringCopyrightFee.setScale(0, RoundingMode.HALF_UP)
+        val foreignNetPayment =
+            krwRoundedNrFee.divide(summary.exchangeRate, foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
+        val foreignWithHoldingTax: BigDecimal =
+            foreignNetPayment.divide(BigDecimal(0.85), foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
+                .subtract(foreignNetPayment)
+        val foreignIncomeTax = foreignWithHoldingTax.divide(
+            BigDecimal(1.1),
+            foreignAmountScaleByCurrency,
+            RoundingMode.HALF_UP
+        )
+        val foreignResidentTax = foreignIncomeTax.multiply(BigDecimal(0.1))
+            .setScale(foreignAmountScaleByCurrency, RoundingMode.HALF_UP)
+        val foreignTotalPayment = foreignNetPayment.add(foreignIncomeTax).add(foreignResidentTax)
+        val krwNetPayment = foreignNetPayment.multiply(summary.exchangeRate)
+            .setScale(0, RoundingMode.HALF_UP)
+        val krwIncomeTax = foreignIncomeTax.multiply(summary.exchangeRate)
+            .setScale(0, RoundingMode.HALF_UP)
+        val krwResidentTax = foreignResidentTax.multiply(summary.exchangeRate)
+            .setScale(0, RoundingMode.HALF_UP)
+        val krwTotalPayment = krwNetPayment.add(krwIncomeTax).add(krwResidentTax)
+        val currencyTranslationProfit = krwNetPayment.subtract(krwRoundedNrFee).toInt()
+
+        return Output(
+            summary.settlementMonth,
+            summary.rightHolderName,
+            summary.neighboringCopyrightFee,
+            summary.currency,
+            summary.exchangeRate,
+            summary.nation,
+            summary.withholdingTaxBurdenType,
+            summary.withholdingTaxRate,
+            foreignTotalPayment,
+            foreignIncomeTax,
+            foreignResidentTax,
+            foreignNetPayment,
+            currencyTranslationProfit,
+            krwRoundedNrFee,
+            krwTotalPayment,
+            krwIncomeTax,
+            krwResidentTax,
+            krwNetPayment,
+        )
     }
 }
